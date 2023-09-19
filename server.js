2cd26aced86153c205b3a3b799d062328a6933e3
Ruban Gino Singh <rubangino22@gmail.com>
2023-09-19 22:17:06 +0530

    ADD Backend

--- server.js
+++ server.js
@@ -12,12 +12,18 @@ io.on("connection", function (socket) {
     socket.on("sender-join", function (data) {
         socket.join(data.uid);
     });
-    socket.on("reciever-join", function (data) {
+    socket.on("receiver-join", function (data) {
         socket.join(data.uid);  
         socket.in(data.sender_uid).emit("init", data.uid);
     });
     socket.on("file-meta", function (data) {
         socket.in(data.uid).emit("fs-meta", data.metadata);
+    });
+    socket.on("fs-start", function (data) {
+        socket.in(data.uid).emit('fs-share', {});  
+    });
+    socket.on("file-raw", function (data) {
+        socket.in(data.uid).emit("fs-share", data.buffer);
     });
 });
 
